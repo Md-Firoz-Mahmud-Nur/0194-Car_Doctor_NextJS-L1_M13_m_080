8e2ba2c56577d02c7d6c9a116b981a9b23dd1445
firoz.nur <firoznur5@gmail.com>
2025-07-08 23:22:17 +0600

    Fix : Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const navItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar container mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -26,52 +50,45 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItems.map((item) => (
+              <li key={item.title}>
+                <Link href={item.path}>{item.title}</Link>
+              </li>
+            ))}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+
+        <Link href="/">
+          <Image src="/assets/logo.svg" alt="logo" width={107} height={87} />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+          {navItems.map((item) => (
+            <li key={item.title}>
+              <Link href={item.path}>{item.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <Image
+          className="mr-5"
+          src="/assets/Cart.svg"
+          alt="cart"
+          width={24}
+          height={24}
+        ></Image>
+        <Image
+          className="mr-8"
+          src="/assets/Search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        ></Image>
+        <a className="btn btn-outline btn-primary px-7 py-4 text-lg font-semibold">
+          Appointment
+        </a>
       </div>
     </div>
   );
